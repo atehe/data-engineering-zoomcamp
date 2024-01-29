80074e1e7d91a27445318f31d166a03a6413847e
Stephen Atehe <atehestephen@gmail.com>
2024-01-29 16:02:24 +0100

    Add bq and bucket terraform

---
+++ Module 1/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "de-projects-383809"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "module_1_bucket" {
+  name          = "de-383809_module_1_bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "de_383809_module_1"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
